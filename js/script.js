ab91c90e9dcbb7f862cc23685522ffa7d2021f14
Dmytro <pokintelicad@gmail.com>
2017-07-04 23:01:52 +0300

    parser start

--- js/script.js
+++ js/script.js
@@ -3,22 +3,44 @@ $(document).ready(function () {
   var expressionData = '0';
   var expressionDataView = $('#display');
   var fullExpression = $('#full');
+  var errorExpression = false;
   var setDisplay = function () {
     expressionDataView.html(expressionData);
-    if(expressionData.length > 45) {
+    if(expressionData.length > 25) {
       fullExpression.html(expressionData);
     } else {
       fullExpression.html('');
     }
   };
   setDisplay();
+  var isNumber = function(keyValue) {
+    if(keyValue.charCodeAt(0) > 47 && keyValue.charCodeAt(0) < 58 || keyValue.charCodeAt(0) == 44) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 /*parser start*/
 var myParser =  function () {
-  var expressionForParse = expressionData;
-  for(var i = 0; i < expressionData.length; i++) {
+  var expressionForParse = expressionData.split(' ');
+  //string to array
+
+  console.log(expressionForParse)
+  for(var i = 0; i < expressionForParse.length; i++) {
+    if(expressionForParse[i] == "" && i != expressionForParse.length - 1) {
+      if(expressionForParse[i + 1] != '(') {
+          errorExpression = true;
+      } else {
+        expressionForParse.splice(i, 1);
+      }
+    }
     console.log(expressionForParse[i])
+
   }
   setDisplay();
+  if(errorExpression) {
+    $('.error').show();
+  }
 }
 /*parser end*/
 /* key listener start*/
@@ -26,6 +48,7 @@ var myParser =  function () {
     var clickValue = $(this).attr('value');
 
     if(clickValue == 'backspace') {
+      $('.error').hide();
       if(expressionData !== '0') {
         if(expressionData.length != 1) {
           expressionData = expressionData.slice(0, -1);
@@ -36,6 +59,7 @@ var myParser =  function () {
       }
     } else {
       if(clickValue == 'C') {
+        $('.error').hide();
         expressionData = '0';
         setDisplay();
       }else {
@@ -43,16 +67,19 @@ var myParser =  function () {
           myParser();
         } else {
           if(expressionData == '0') {
-            expressionData = $(this).attr('value');
+            if(isNumber(clickValue))
+            expressionData = clickValue;
           } else {
-            expressionData = expressionData + ' ' + $(this).attr('value');
+            if(isNumber(clickValue)) {
+              expressionData = expressionData + clickValue;
+            } else {
+              expressionData = expressionData + ' ' + clickValue + ' ';
+            }
           }
           setDisplay();
         }
-
       }
     }
-
   });
   /* key listener end*/
 });
