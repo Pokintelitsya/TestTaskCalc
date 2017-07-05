dc4d907142a46c899e438146ed7ca61efe1d7b54
Dmytro <pokintelicad@gmail.com>
2017-07-05 12:59:09 +0300

    task end

--- js/script.js
+++ js/script.js
@@ -14,28 +14,89 @@ $(document).ready(function () {
   };
   setDisplay();
   var isNumber = function(keyValue) {
-    if(keyValue.charCodeAt(0) > 47 && keyValue.charCodeAt(0) < 58 || keyValue.charCodeAt(0) == 44) {
+    if(keyValue.charCodeAt(0) > 47 && keyValue.charCodeAt(0) < 58 || keyValue.charCodeAt(0) == 44 || keyValue.charCodeAt(0) == 46) {
       return true;
     } else {
       return false;
     }
   }
+
+
 /*parser start*/
 var myParser =  function () {
+
+  if(expressionData.match(/\(/g) || expressionData.match(/\)/g)) {
+    if(expressionData.match(/\(/g) && expressionData.match(/\)/g)) {
+      if(expressionData.match(/\(/g).length != expressionData.match(/\)/g).length) {
+        errorExpression = true;
+      }
+    } else {
+      errorExpression = true;
+    }
+  }
+
   var expressionForParse = expressionData.split(' ');
   //string to array
-
-  console.log(expressionForParse)
   for(var i = 0; i < expressionForParse.length; i++) {
     if(expressionForParse[i] == "" && i != expressionForParse.length - 1) {
-      if(expressionForParse[i + 1] != '(') {
+      if(expressionForParse[i + 1] != '(' && expressionForParse[i - 1] != ')') {
           errorExpression = true;
       } else {
         expressionForParse.splice(i, 1);
       }
     }
-    console.log(expressionForParse[i])
+    if(expressionForParse[i] == "" && i == expressionForParse.length - 1) {
+      expressionForParse.splice(i, 1);
+    }
+  }
 
+  var withoutBrackets = function (expr) {
+    if(expr.indexOf('*') != -1) {
+      while (expr.indexOf('*') > -1) {
+        var elemIndex = expr.indexOf('*');
+        var results = parseFloat(expr[elemIndex - 1]) * parseFloat(expr[elemIndex + 1]);
+        expr.splice(elemIndex-1, 3, results );
+      }
+    }
+    if(expr.indexOf('/') != -1) {
+      while (expr.indexOf('/') > -1) {
+        var elemIndex = expr.indexOf('/');
+        var results = parseFloat(expr[elemIndex - 1]) / parseFloat(expr[elemIndex + 1]);
+        expr.splice(elemIndex-1, 3, results );
+      }
+    }
+    if(expr.indexOf('+') != -1) {
+      while (expr.indexOf('+') > -1) {
+        var elemIndex = expr.indexOf('+');
+        var results = parseFloat(expr[elemIndex - 1]) + parseFloat(expr[elemIndex + 1]);
+        expr.splice(elemIndex-1, 3, results );
+      }
+    }
+    if(expr.indexOf('-') != -1) {
+      while (expr.indexOf('-') > -1) {
+        var elemIndex = expr.indexOf('-');
+        var results = parseFloat(expr[elemIndex - 1]) - parseFloat(expr[elemIndex + 1]);
+        expr.splice(elemIndex-1, 3, results );
+      }
+    }
+    return expr;
+  }
+
+  var withBrackets = function (expr) {
+    if(expr.indexOf('(') != -1) {
+      while (expr.indexOf('(') > -1) {
+        var openPosition = expr.lastIndexOf('(');
+        var closePosition = expr.indexOf(')');
+        var exprInBrackets = expr.splice(openPosition + 1, closePosition - openPosition - 1);
+        expr.splice(openPosition, exprInBrackets.length + 1, '' + withoutBrackets(exprInBrackets)[0])
+      }
+    }
+    expr =  withoutBrackets(expr);
+    return expr;
+  };
+  
+  if(!errorExpression) {
+    expressionData = withBrackets(expressionForParse)
   }
   setDisplay();
   if(errorExpression) {
@@ -43,6 +104,7 @@ var myParser =  function () {
   }
 }
 /*parser end*/
+
 /* key listener start*/
   $('.key').on('click', function () {
     var clickValue = $(this).attr('value');
@@ -51,7 +113,12 @@ var myParser =  function () {
       $('.error').hide();
       if(expressionData !== '0') {
         if(expressionData.length != 1) {
-          expressionData = expressionData.slice(0, -1);
+          if(expressionData[expressionData.length - 1] != ' ') {
+            expressionData = expressionData.slice(0, -1);
+          } else {
+            expressionData = expressionData.slice(0, -3);
+          }
+
         } else {
           expressionData = '0';
         }
